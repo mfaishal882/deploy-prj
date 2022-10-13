d13a5e722e569f2aecb6a91651c1152b2bbb3018
mfaishal882 <mfaishal882@gmail.com>
2022-10-13 16:45:21 +0700

    add hello_gaes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,9 +10,14 @@ import (
 
 func main() {
 	e := echo.New()
+	log.Print("\n\n\n")
 	e.Use(middleware.Logger())
+	log.Print("\n\n\n")
 	e.GET("/hello", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "hello world")
+	})
+	e.GET("/hello_gaes", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hello gaes from wonderland")
 	})
 	e.Start(":8000")
 }
